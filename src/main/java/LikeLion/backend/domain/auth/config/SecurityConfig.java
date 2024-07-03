576f7bbb22851770b7e3f541e566a5286b790b53
siwan9 <si.wan208@gmail.com>
2024-07-04 01:14:44 +0900

    [refactor] CORS 및 필터 대상 요청 지정

--- src/main/java/LikeLion/backend/domain/auth/config/SecurityConfig.java
+++ src/main/java/LikeLion/backend/domain/auth/config/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -27,16 +32,34 @@ public class SecurityConfig {
     public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors((cors)->{cors.configurationSource(corsConfigurationSource());
+                })
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(auth -> auth
                         .maximumSessions(1)
                         .maxSessionsPreventsLogin(false))
                 .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/logout").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/board").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/board").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/board").authenticated()
                         .anyRequest().permitAll())
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        //configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean // PasswordEncoder
